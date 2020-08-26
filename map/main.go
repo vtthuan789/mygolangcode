52f8d7bc0226608a44741782d98e3dd27610d57d
Thuan Vo <vothanhthuan7990@gmail.com>
2020-08-26 21:30:44 +0700

    First map project

---
+++ map/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+
+	colors := map[string]string{
+		"red":    "#ff0000",
+		"yellow": "#174128",
+		"blue":   "#743847",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
