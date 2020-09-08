e8f74fa6724bd2372f954ff8c28922248bde0c5f
Thuan Vo <vothanhthuan7990@gmail.com>
2020-09-08 15:16:12 +0700

    Implement Greet RPC Unary API

--- grpc_go/greet/greet_client/client.go
+++ grpc_go/greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"grpc_go/greet/greetpb"
 	"log"
@@ -21,5 +22,26 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(cc)
 
-	fmt.Printf("Create client: %f", c)
+	// fmt.Printf("Create client: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Start doing a Unary RPC...")
+
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Tony",
+			LastName:  "Stark",
+		},
+	}
+
+	res, err := c.Greet(context.Background(), req)
+
+	if err != nil {
+		log.Fatalln("Error while calling Greet RPC:", err)
+	}
+
+	fmt.Println("Receive response from Greet service:", res)
 }
--- grpc_go/greet/greet_server/server.go
+++ grpc_go/greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -11,6 +12,15 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet() function is calling with %v\n", req)
+	result := "Hello " + req.GetGreeting().GetFirstName()
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
 	fmt.Println("Hello world!")
