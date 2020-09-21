41a96f82e7ee21ecaaf80c9d46c53fa0282a99e8
Thuan Vo <vothanhthuan7990@gmail.com>
2020-09-22 01:12:33 +0700

    Implement Greet Server Streaming GRPC

--- grpc_go/greet/greet_client/client.go
+++ grpc_go/greet/greet_client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"grpc_go/greet/greetpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -25,6 +26,7 @@ func main() {
 	// fmt.Printf("Create client: %f", c)
 
 	doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -44,4 +46,36 @@ func doUnary(c greetpb.GreetServiceClient) {
 	}
 
 	fmt.Println("Receive response from Greet service:", res)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Start doing a Sever Streaming RPC...")
+
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Iron",
+			LastName:  "Man",
+		},
+	}
+
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+
+	if err != nil {
+		log.Fatalln("Error while calling GreetManyTimes RPC:", err)
+	}
+
+	for {
+		msg, err := resStream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalln("Error while reading stream:", err)
+		}
+
+		fmt.Println("Receive response from GreetManyTimes service:", msg.GetResult())
+	}
+
 }
