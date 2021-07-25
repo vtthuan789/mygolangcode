2668be1aee641f0de4211eec3001b3496b82d413
Thuan Vo <vothanhthuan7990@gmail.com>
2021-07-25 16:44:42 +0700

    Learn about variables

---
+++ gocrashcourse/variables/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const prompt = "and don't type your number in, just press ENTER when ready."
+
+func main() {
+	// seed the random generator
+	rand.Seed(time.Now().UnixNano())
+
+	firstNumber := rand.Intn(8) + 2
+	secondNumber := rand.Intn(8) + 2
+	subtraction := rand.Intn(8) + 2
+	answer := firstNumber*secondNumber - subtraction
+
+	playTheGame(firstNumber, secondNumber, subtraction, answer)
+}
+
+func playTheGame(firstNumber, secondNumber, subtraction, answer int) {
+	reader := bufio.NewReader(os.Stdin)
+
+	// Display a welcome/instructions
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	// take them through the games
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	// give them the answer
+	fmt.Println("The answer is", answer)
+}
