969510e29b7d7492338f74e8262521e90eeb2c4a
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-02 09:59:50 +0700

    Testing go module

---
+++ gocrashcourse/play-ground/go.mod
@@ -0,0 +1,3 @@
+module github.com/vtthuan789/mygolangcode/tree/master/gocrashcourse
+
+go 1.15
---
+++ gocrashcourse/play-ground/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/vtthuan/mygolangcode/gocrashcourse/play-ground/helpers"
+)
+
+func main() {
+	var myType helpers.SomeType
+	myType.TypeString = "Some string"
+	myType.TypeInt = 79
+	log.Println(myType)
+	// you can't do this in golang
+	// var myString * string
+	// myString  = "Read"
+	var myString = "Read"
+	log.Println("Before:", myString)
+	log.Println("Adress Before:", &myString)
+	changeString(&myString)
+	// myString = "Write"
+	log.Println("After:", myString)
+	log.Println("Adress After:", &myString)
+}
+
+func changeString(s *string) {
+	*s = "Write"
+}
