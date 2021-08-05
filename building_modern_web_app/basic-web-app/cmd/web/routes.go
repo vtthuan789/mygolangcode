eddf6897856b3e402522e7f722dde288c2285da0
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-05 22:11:27 +0700

    Using chi for routing

--- building_modern_web_app/basic-web-app/cmd/web/routes.go
+++ building_modern_web_app/basic-web-app/cmd/web/routes.go
@@ -3,16 +3,15 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app/pkg/config"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- building_modern_web_app/basic-web-app/go.mod
+++ building_modern_web_app/basic-web-app/go.mod
@@ -2,4 +2,4 @@ module github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app
 
 go 1.15
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+require github.com/go-chi/chi/v5 v5.0.3
