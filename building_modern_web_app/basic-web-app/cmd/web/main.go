726ec5c791be7d9af834bbb36df814f316524d34
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-05 23:01:33 +0700

    Setting up a session package

--- building_modern_web_app/basic-web-app/cmd/web/main.go
+++ building_modern_web_app/basic-web-app/cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app/pkg/config"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app/pkg/handlers"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app/pkg/render"
@@ -12,9 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- building_modern_web_app/basic-web-app/go.mod
+++ building_modern_web_app/basic-web-app/go.mod
@@ -3,6 +3,7 @@ module github.com/vtthuan789/mygolangcode/building_modern_web_app/basic-web-app
 go 1.15
 
 require (
+	github.com/alexedwards/scs/v2 v2.4.0
 	github.com/go-chi/chi/v5 v5.0.3
 	github.com/justinas/nosurf v1.1.1
 )
