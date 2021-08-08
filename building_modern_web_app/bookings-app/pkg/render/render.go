ad7cc4c8e24717f0ae7feb748226ea431365ad4b
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-08 22:13:34 +0700

    Create form handler and add CSRF protection

--- building_modern_web_app/bookings-app/pkg/render/render.go
+++ building_modern_web_app/bookings-app/pkg/render/render.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/pkg/config"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/pkg/models"
 )
@@ -21,13 +22,14 @@ func NewTemplate(a *config.AppConfig) {
 }
 
 // AddDefaultData adds default data to template data
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
 	// Add data you want to apprear in every page
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc = map[string]*template.Template{}
 	if app.UseCache {
 		tc = app.TemplateCache
@@ -41,7 +43,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	}
 
 	buf := new(bytes.Buffer)
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
