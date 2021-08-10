817145a65b7eacbb3f451a12892935136d69c635
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-10 09:53:41 +0700

    Add alerts when user enters reservation-summary page without posting a reservation

--- building_modern_web_app/bookings-app/internal/handlers/handlers.go
+++ building_modern_web_app/bookings-app/internal/handlers/handlers.go
@@ -154,9 +154,14 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		log.Println("Cannot get item from session")
+		log.Println("Cannot get item from session!")
+		m.App.Session.Put(r.Context(), "error", "Cannot get reservation from session!")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
+
+	m.App.Session.Remove(r.Context(), "reservation")
+
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
 
--- building_modern_web_app/bookings-app/internal/render/render.go
+++ building_modern_web_app/bookings-app/internal/render/render.go
@@ -24,6 +24,9 @@ func NewTemplate(a *config.AppConfig) {
 // AddDefaultData adds default data to template data
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
 	// Add data you want to apprear in every page
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
 	td.CSRFToken = nosurf.Token(r)
 	return td
 }
