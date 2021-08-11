01f312212e80334a307cf54bf530cdf8b6648d85
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-12 00:24:35 +0700

    Test AddDefaultData in render package

---
+++ building_modern_web_app/bookings-app/internal/render/setup_test.go
@@ -0,0 +1,35 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/config"
+	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/models"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// Register what is going to be put in sesstion
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+	os.Exit(m.Run())
+}
