dc049bb666ca0ec6a498c7662ff4e894556bd496
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-12 22:45:54 +0700

    Creating helpers package with error handling functions

--- building_modern_web_app/bookings-app/cmd/web/main.go
+++ building_modern_web_app/bookings-app/cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/config"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/handlers"
+	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/helpers"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/models"
 	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main function
 func main() {
@@ -45,6 +49,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -67,6 +77,7 @@ func run() error {
 
 	render.NewTemplate(&app)
 
+	helpers.NewHelpers(&app)
 	// http.HandleFunc("/", handlers.Repo.Home)
 	// http.HandleFunc("/about", handlers.Repo.About)
 	return nil
