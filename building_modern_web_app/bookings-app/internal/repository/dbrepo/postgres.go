bc213cd3169b29b734888b59f3acc9a282addb80
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-17 00:19:23 +0700

    Create functions to search for availability to database repo

--- building_modern_web_app/bookings-app/internal/repository/dbrepo/postgres.go
+++ building_modern_web_app/bookings-app/internal/repository/dbrepo/postgres.go
@@ -57,3 +57,55 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 
 	return err
 }
+
+// SearchAvailabilityByDatesAndRoomID returns true if availability exists for roomID, otherwise false
+func (m *postgresDBRepo) SearchAvailabilityByDatesAndRoomID(
+	start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `select count(id) from room_restrictions
+		where room_id = $1 and $2 < end_date and $3 > start_date`
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+
+	if err == nil && numRows == 0 {
+		return true, nil
+	}
+	return false, err
+}
+
+// SearchAvailabilityForAllRooms returns a slice of available rooms, if any for a given date range
+func (m *postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var rooms []models.Room
+
+	query := `select r.id, r.room_name from rooms r
+		where r.id not in (select room_id from room_restrictions rr 
+		where $1 < end_date and $2 > start_date`
+
+	rows, err := m.DB.QueryContext(ctx, query, start, end)
+	if err != nil {
+		return rooms, err
+	}
+
+	for rows.Next() {
+		var room models.Room
+		err = rows.Scan(&room.ID, &room.RoomName)
+		if err != nil {
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+
+	return rooms, nil
+}
--- building_modern_web_app/bookings-app/internal/repository/repository.go
+++ building_modern_web_app/bookings-app/internal/repository/repository.go
@@ -1,10 +1,16 @@
 package repository
 
-import "github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/models"
+import (
+	"time"
+
+	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(models.Reservation) (int, error)
 	InsertRoomRestriction(models.RoomRestriction) error
+	SearchAvailabilityByDatesAndRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
