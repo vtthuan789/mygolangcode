e33937f065d819978eb1a42c17844a85636afad4
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-19 22:13:58 +0700

    Create a test database repo

---
+++ building_modern_web_app/bookings-app/internal/repository/dbrepo/test-repo.go
@@ -0,0 +1,38 @@
+package dbrepo
+
+import (
+	"time"
+
+	"github.com/vtthuan789/mygolangcode/building_modern_web_app/bookings-app/internal/models"
+)
+
+func (m *testDBRepo) AllUsers() bool {
+	return true
+}
+
+func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
+	return 1, nil
+}
+
+// InsertRoomRestriction inserts a room restriction into database
+func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	return nil
+}
+
+// SearchAvailabilityByDatesAndRoomID returns true if availability exists for roomID, otherwise false
+func (m *testDBRepo) SearchAvailabilityByDatesAndRoomID(
+	start, end time.Time, roomID int) (bool, error) {
+	return false, nil
+}
+
+// SearchAvailabilityForAllRooms returns a slice of available rooms, if any for a given date range
+func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	var rooms []models.Room
+	return rooms, nil
+}
+
+// GetRoomByID gets a room by ID
+func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
+	var room models.Room
+	return room, nil
+}
