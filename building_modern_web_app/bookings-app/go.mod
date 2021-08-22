7391286b2bc18723810de7ba05a0186a21d1d9b7
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-22 21:08:26 +0700

    Create authenticate and user database functions

--- building_modern_web_app/bookings-app/go.mod
+++ building_modern_web_app/bookings-app/go.mod
@@ -28,4 +28,5 @@ require (
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/spf13/cobra v1.2.1 // indirect
 	github.com/xhit/go-simple-mail/v2 v2.10.0
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 )
--- building_modern_web_app/bookings-app/internal/repository/dbrepo/test-repo.go
+++ building_modern_web_app/bookings-app/internal/repository/dbrepo/test-repo.go
@@ -90,3 +90,23 @@ func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
 	}
 	return room, nil
 }
+
+// GetUserByID gets a user by ID
+func (m *testDBRepo) GetUserByID(id int) (models.User, error) {
+	var u models.User
+
+	return u, nil
+}
+
+// UpdateUser updates a user in the database
+func (m *testDBRepo) UpdateUser(u models.User) error {
+	return nil
+}
+
+// Authenticate authenticates a user
+func (m *testDBRepo) Authenticate(email, testPassword string) (int, string, error) {
+	var id int
+	var hashedPassword string
+
+	return id, hashedPassword, nil
+}
