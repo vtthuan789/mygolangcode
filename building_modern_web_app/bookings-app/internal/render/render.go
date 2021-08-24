5b37d4fff009596dfa80c1adc73cc75879caa367
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-24 23:34:14 +0700

    Show reservation calendar

--- building_modern_web_app/bookings-app/internal/render/render.go
+++ building_modern_web_app/bookings-app/internal/render/render.go
@@ -15,7 +15,10 @@ import (
 )
 
 var functions = template.FuncMap{
-	"humanDate": HumanDate,
+	"humanDate":  HumanDate,
+	"formatDate": FormatDate,
+	"iterate":    Iterate,
+	"add":        Add,
 }
 
 var app *config.AppConfig
@@ -29,6 +32,25 @@ func NewRenderer(a *config.AppConfig) {
 // HumanDate returns time in YYYY-MM-DD format
 func HumanDate(t time.Time) string {
 	return t.Format("2006-01-02")
+}
+
+//FormatDate returns the formatted date
+func FormatDate(t time.Time, f string) string {
+	return t.Format(f)
+}
+
+// Iterate return a slice of ints, starting at 0, going to count
+func Iterate(count int) []int {
+	var i int
+	var items []int
+	for i = 0; i < count; i++ {
+		items = append(items, i)
+	}
+	return items
+}
+
+func Add(a, b int) int {
+	return a + b
 }
 
 // AddDefaultData adds default data to template data
