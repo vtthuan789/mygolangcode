035d437846dd632eee2089bda9373ca147dc3936
Thuan Vo <vothanhthuan7990@gmail.com>
2021-08-24 10:59:56 +0700

    Create handler to process a reservation

--- building_modern_web_app/bookings-app/internal/handlers/handlers.go
+++ building_modern_web_app/bookings-app/internal/handlers/handlers.go
@@ -610,3 +610,24 @@ func (m *Repository) AdminPostShowReservation(w http.ResponseWriter, r *http.Req
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
 }
+
+// AdminProcessReservation marks a reservation as processed
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	exploded := strings.Split(r.RequestURI, "/")
+	id, err := strconv.Atoi(exploded[4])
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	src := exploded[3]
+
+	err = m.DB.UpdateProcessedForReservation(id, 1) // 1 means "processed"
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
+}
