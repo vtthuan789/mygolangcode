1e6cc888b09f1f15adc6bb303ffffab9774701f5
Thuan Vo <vothanhthuan7990@gmail.com>
2021-09-03 21:21:42 +0700

    Add OPTIONS method

--- go-httpclient/gohttp/client.go
+++ go-httpclient/gohttp/client.go
@@ -20,6 +20,7 @@ type Client interface {
 	Put(url string, headers http.Header, body interface{}) (*core.Response, error)
 	Patch(url string, headers http.Header, body interface{}) (*core.Response, error)
 	Delete(url string, headers http.Header) (*core.Response, error)
+	Options(url string, headers http.Header) (*core.Response, error)
 }
 
 func (c *httpClient) Get(url string, headers http.Header) (*core.Response, error) {
@@ -40,4 +41,8 @@ func (c *httpClient) Patch(url string, headers http.Header, body interface{}) (*
 
 func (c *httpClient) Delete(url string, headers http.Header) (*core.Response, error) {
 	return c.do(http.MethodDelete, url, headers, nil)
+}
+
+func (c *httpClient) Options(url string, headers http.Header) (*core.Response, error) {
+	return c.do(http.MethodOptions, url, headers, nil)
 }
