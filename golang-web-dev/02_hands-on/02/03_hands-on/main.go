0034a276b3d95c5f18b3c7432ae5c91d2cbf2c3c
Thuan Vo <vothanhthuan7990@gmail.com>
2021-09-16 21:15:58 +0700

    Hands on routing 02 03

---
+++ golang-web-dev/02_hands-on/02/03_hands-on/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Println(line)
+		}
+
+		fmt.Println("Code got here.")
+		_, err = io.WriteString(conn, "I see you connected.")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		conn.Close()
+	}
+}
