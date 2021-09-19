ed6b8611423247dcb92b63411c32a25040224109
Thuan Vo <vothanhthuan7990@gmail.com>
2021-09-19 10:13:49 +0700

    Hands on serving files 07

---
+++ golang-web-dev/03_hands-on/07_hands-on/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFiles("starting-files/templates/index.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl.Execute(w, nil)
+}
+
+func main() {
+
+	http.HandleFunc("/", foo)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("starting-files/public"))))
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
